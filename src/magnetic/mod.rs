512d022c9272c786992302fdf05a67596140663c
Yijun Tang <yjtangphy@gmail.com>
2025-12-01 15:21:08 +0000

    mut in magnetic mod module

--- src/magnetic/mod.rs
+++ src/magnetic/mod.rs
@@ -96,7 +96,7 @@ impl<'a> System<'a> for CalculateMagneticFieldMagnitudeSystem {
     fn run(&mut self, mut sampler: Self::SystemData) {
         use rayon::prelude::*;
 
-        (&mut sampler).par_join().for_each(|sampler| {
+        (&mut sampler).par_join().for_each(|mut sampler| {
             sampler.magnitude = sampler.field.norm();
             if sampler.magnitude.is_nan() {
                 sampler.magnitude = 0.0;
@@ -115,7 +115,7 @@ impl<'a> System<'a> for CalculateMagneticMagnitudeGradientSystem {
     fn run(&mut self, mut sampler: Self::SystemData) {
         use rayon::prelude::*;
 
-        (&mut sampler).par_join().for_each(|sampler| {
+        (&mut sampler).par_join().for_each(|mut sampler| {
             let mut gradient = Vector3::new(0.0, 0.0, 0.0);
             for i in 0..3 {
                 gradient[i] =
